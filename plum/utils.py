7b75a6d6f29d05d2716d341c2f3c192fcb908954
Martin Uhrin <martin.uhrin@gmail.com>
2017-09-07 13:16:06 +0200

    Reusing apricopy's SimpleNamespace

--- plum/utils.py
+++ plum/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import abc
+import apricotpy.utils
 from apricotpy import persistable
 import frozendict
 import importlib
@@ -177,21 +177,7 @@ class AttributesFrozendict(frozendict.frozendict):
         return self.keys()
 
 
-class SimpleNamespace(object):
-    """
-    An attempt to emulate python 3's types.SimpleNamespace
-    """
-
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
-
-    def __repr__(self):
-        keys = sorted(self.__dict__)
-        items = ("{}={!r}".format(k, self.__dict__[k]) for k in keys)
-        return "{}({})".format(type(self).__name__, ", ".join(items))
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
+SimpleNamespace = apricotpy.utils.SimpleNamespace
 
 
 def load_with_classloader(bundle):
