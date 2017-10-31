3a58030222ddb19957aa85195c1a32a99782eea3
Sebastiaan Huber <mail@sphuber.net>
2017-10-31 17:30:22 +0100

    New passive implementation of a Persister class
    The new Persister implementation is passive in the sense
    that it no longer is listening to state transitions of a
    Process, but needs to be actively called when it needs to
    persist a process. The abstract class defines a save and
    a load_checkpoint method. A simple PicklePersister implementation
    uses pickles written to disk to store the checkpoints

--- plum/persistence/__init__.py
+++ plum/persistence/__init__.py
@@ -1,3 +1,27 @@
+from abc import abstractmethod, abstractproperty, ABCMeta
+import apricotpy
 
 
-from plum.persistence.bundle import Bundle
+class Persister(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def save_checkpoint(self, process):
+        """
+        Persist a Process instance
+
+        :param process: :class:`plum.process.Process`
+        """
+        pass
+
+    @abstractmethod
+    def load_checkpoint(self, pid):
+        """
+        Load a process from a persisted checkpoint by its process id
+
+        :param pid: the process id of the :class:`plum.process.Process`
+        :return: a bundle with the process state
+        :rtype: :class:`apricotpy.Bundle`
+        """
+        pass
---
+++ plum/persistence/pickle.py
@@ -0,0 +1,83 @@
+import os
+import errno
+import pickle
+
+from apricotpy.persistable import Bundle
+from plum.persistence import Persister
+
+
+class PicklePersister(Persister):
+    """
+    Implementation of the abstract Persister class that stores Process states
+    in pickles on a filesystem.
+    """
+
+    def __init__(self, pickle_directory):
+        """
+        Instantiate a PicklePersister object that will persist processes by
+        writing their bundles to a pickle in a directory specified by the
+        argument 'pickle_directory'
+
+        :param pickle_directory: the full path to the directory where pickles will be written
+        """
+        super(PicklePersister, self).__init__()
+
+        try:
+            ensure_pickle_directory(pickle_directory)
+        except OSError as exception:
+            raise ValueError('failed to create the pickle directory at {}'.format(pickle_directory))
+
+        self._pickle_directory = pickle_directory
+
+
+    @staticmethod
+    def ensure_pickle_directory(dirpath):
+        """
+        Will attempt to create the directory at dirpath and raise if it fails, except
+        if the exception arose because the directory already existed
+        """
+        try:
+            os.makedirs(dirpath)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
+    @staticmethod
+    def pickle_filename(pid):
+        """
+        Returns the relative filepath of the pickle for the given process id
+        """
+        return '{}.pickle'.format(pid)
+
+    @staticmethod
+    def pickle_filepath(pid):
+        """
+        Returns the full filepath of the pickle for the given process id
+        """
+        return os.path.join(self._pickle_directory, pickle_filename(pid))
+
+    def save_checkpoint(self, process):
+        """
+        Persist a process to a pickle on disk
+
+        :param process: :class:`plum.process.Process`
+        """
+        bundle = Bundle(process)
+
+        with open(pickle_filepath(process.pid), 'w+b') as handle:
+            pickle.dump(bundle, handle)
+
+    def load_checkpoint(self, pid):
+        """
+        Load a process from a persisted checkpoint by its process id
+
+        :param pid: the process id of the :class:`plum.process.Process`
+        :return: a bundle with the process state
+        :rtype: :class:`apricotpy.persistable.Bundle`
+        """
+        try:
+            bundle = pickle.load(pickle_filepath(pid))
+        except IOError as exception:
+            raise
+
+        return bundle
