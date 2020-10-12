67dcdbf14e9624bc0c477be7ab3a76521c2d43f6
Sebastiaan Huber <mail@sphuber.net>
2020-10-12 16:40:20 +0200

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='workflow multithreaded rabbitmq',
     python_requires='>=3.5',
