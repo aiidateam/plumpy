97b7a0c3b6a550979b9a93ee3b5f86cf13dafcc4
Simon Adorf <simon.adorf@epfl.ch>
2022-04-06 09:42:44 +0200

    🔧 MAINTAIN: Drop support for Python 3.6.

--- setup.py
+++ setup.py
@@ -22,13 +22,12 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     keywords='workflow multithreaded rabbitmq',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'pyyaml~=5.4', 'nest_asyncio~=1.4.0', 'aio-pika~=6.6', 'aiocontextvars~=0.2.2; python_version<"3.7"',
         'kiwipy[rmq]~=0.7.4'
