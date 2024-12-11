981fa91f754c503412caebb74f922f6d5fdcd630
Jusong Yu <jusong.yeu@gmail.com>
2024-12-11 20:59:07 +0100

    Move communication into rmq module
    Move communication to rmq

--- src/plumpy/rmq/__init__.py
+++ src/plumpy/rmq/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from .exceptions import *
+from .futures import *
+from .process_comms import *
 
-__all__ = exceptions.__all__
+__all__ = exceptions.__all__ + communications.__all__ + futures.__all__ + process_comms.__all__
