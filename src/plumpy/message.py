767a27b17be7ae4c330b01515399ffb2d6ac3e23
Jusong Yu <jusong.yeu@gmail.com>
2024-12-14 14:27:28 +0100

    Move TaskRejectError as the common exception for task launch

--- src/plumpy/message.py
+++ src/plumpy/message.py
@@ -8,6 +8,8 @@ from typing import TYPE_CHECKING, Any, Dict, Optional, Sequence, Union, cast
 from plumpy.coordinator import Communicator
 from plumpy.exceptions import PersistenceError, TaskRejectedError
 
+from plumpy.exceptions import PersistenceError, TaskRejectedError
+
 from . import loaders, persistence
 from .utils import PID_TYPE
 
