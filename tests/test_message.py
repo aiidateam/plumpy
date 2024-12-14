4dae773dd91b9081df6b7a32dbf255f408156981
Jusong Yu <jusong.yeu@gmail.com>
2024-12-14 14:39:54 +0100

    Remove useless communicator param passed to ProcessLaunch __call__

--- tests/test_message.py
+++ tests/test_message.py
@@ -37,7 +37,7 @@ async def test_continue():
     del process
     process = None
 
-    result = await launcher._continue(None, **plumpy.create_continue_body(pid)[message.TASK_ARGS])
+    result = await launcher._continue(**plumpy.create_continue_body(pid)[message.TASK_ARGS])
     assert result == utils.DummyProcess.EXPECTED_OUTPUTS
 
 
@@ -51,5 +51,5 @@ async def test_loader_is_used():
     launcher = plumpy.ProcessLauncher(persister=persister, loader=loader)
 
     continue_task = plumpy.create_continue_body(proc.pid)
-    result = await launcher._continue(None, **continue_task[message.TASK_ARGS])
+    result = await launcher._continue(**continue_task[message.TASK_ARGS])
     assert result == utils.DummyProcess.EXPECTED_OUTPUTS
