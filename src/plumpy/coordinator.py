34d38427e27bdf064f2b839efce1a1e04e12629b
Jusong Yu <jusong.yeu@gmail.com>
2024-12-17 13:09:52 +0100

    Interface for ProcessController

--- src/plumpy/coordinator.py
+++ src/plumpy/coordinator.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Callable, Pattern, Protocol
+import concurrent.futures
+from typing import TYPE_CHECKING, Any, Callable, Hashable, Pattern, Protocol
 
-RpcSubscriber = Callable[['Communicator', Any], Any]
-BroadcastSubscriber = Callable[['Communicator', Any, Any, Any, Any], Any]
+
+if TYPE_CHECKING:
+    # identifiers for subscribers
+    ID_TYPE = Hashable
+    Subscriber = Callable[..., Any]
+    # RPC subscriber params: communicator, msg
+    RpcSubscriber = Callable[['Coordinator', Any], Any]
+    # Task subscriber params: communicator, task
+    TaskSubscriber = Callable[['Coordinator', Any], Any]
+    # Broadcast subscribers params: communicator, body, sender, subject, correlation id
+    BroadcastSubscriber = Callable[['Coordinator', Any, Any, Any, ID_TYPE], Any]
 
 
 class Communicator(Protocol):
-    def add_rpc_subscriber(self, subscriber: RpcSubscriber, identifier=None) -> Any: ...
+    def add_rpc_subscriber(self, subscriber: 'RpcSubscriber', identifier: 'ID_TYPE | None' = None) -> Any: ...
 
     def add_broadcast_subscriber(
-        self, subscriber: BroadcastSubscriber, subject_filter: str | Pattern[str] | None = None, identifier=None
+        self, subscriber: 'BroadcastSubscriber', subject_filter: str | Pattern[str] | None = None, identifier=None
     ) -> Any: ...
 
     def remove_rpc_subscriber(self, identifier): ...
@@ -17,16 +27,34 @@ class Communicator(Protocol):
     def remove_broadcast_subscriber(self, identifier): ...
 
     def broadcast_send(self, body, sender=None, subject=None, correlation_id=None) -> bool: ...
+
 
 class Coordinator(Protocol):
-    def add_rpc_subscriber(self, subscriber: RpcSubscriber, identifier=None) -> Any: ...
+    def add_rpc_subscriber(self, subscriber: 'RpcSubscriber', identifier=None) -> Any: ...
 
     def add_broadcast_subscriber(
-        self, subscriber: BroadcastSubscriber, subject_filter: str | Pattern[str] | None = None, identifier=None
+        self,
+        subscriber: 'BroadcastSubscriber',
+        subject_filter: str | Pattern[str] | None = None,
+        identifier: 'ID_TYPE | None' = None,
     ) -> Any: ...
+
+    def add_task_subscriber(self, subscriber: 'TaskSubscriber', identifier: 'ID_TYPE | None' = None) -> 'ID_TYPE': ...
 
     def remove_rpc_subscriber(self, identifier): ...
 
     def remove_broadcast_subscriber(self, identifier): ...
 
-    def broadcast_send(self, body, sender=None, subject=None, correlation_id=None) -> bool: ...
+    def remove_task_subscriber(self, identifier: 'ID_TYPE') -> None: ...
+
+    def rpc_send(self, recipient_id: Hashable, msg: Any) -> Any: ...
+
+    def broadcast_send(
+        self,
+        body: Any | None,
+        sender: str | None = None,
+        subject: str | None = None,
+        correlation_id: 'ID_TYPE | None' = None,
+    ) -> Any: ...
+
+    def task_send(self, task: Any, no_reply: bool = False) -> Any: ...
