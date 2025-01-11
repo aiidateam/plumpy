6d3101de0472f6ccf67938a61966f38410f22445
Jusong Yu <jusong.yeu@gmail.com>
2025-01-12 00:22:23 +0100

    Export plumpy.futures.Future

--- src/plumpy/rmq/__init__.py
+++ src/plumpy/rmq/__init__.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 # mypy: disable-error-code=name-defined
-from .communications import *
-from .futures import *
-from .process_control import *
+from .communications import Communicator, DeliveryFailed, RemoteException, TaskRejected, wrap_communicator
+from .futures import unwrap_kiwi_future, wrap_to_concurrent_future
+from .process_control import RemoteProcessController, RemoteProcessThreadController
 
-__all__ = communications.__all__ + futures.__all__ + process_control.__all__
+__all__ = [
+    # communications
+    'Communicator',
+    'DeliveryFailed',
+    'RemoteException',
+    # process_control
+    'RemoteProcessController',
+    'RemoteProcessThreadController',
+    'TaskRejected',
+    # futures
+    'unwrap_kiwi_future',
+    'wrap_communicator',
+    'wrap_to_concurrent_future',
+]
