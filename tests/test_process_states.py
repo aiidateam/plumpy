e1b68d7694ab43b6f0e7c92b2abc823ead82b30b
Jusong Yu <jusong.yeu@gmail.com>
2025-01-22 21:57:14 +0100

    Test to show the default loader can only load obj in python path

--- tests/test_process_states.py
+++ tests/test_process_states.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from typing import Any
 import pytest
 
@@ -31,45 +32,15 @@ def proc() -> 'StateMachine':
     return DummyProcess()
 
 
-def test_create_savable(proc: StateMachine):
-    state = Created(proc, run_fn=lambda: None)
-    assert isinstance(state, Savable)
-
-
-def test_running_savable(proc: StateMachine):
-    state = Running(proc, run_fn=lambda: None)
-    assert isinstance(state, Savable)
-
-
-def test_waiting_savable(proc: StateMachine):
-    state = Waiting(proc, done_callback=lambda: None)
-    assert isinstance(state, Savable)
-
-
-def test_excepted_savable():
-    state = Excepted(exception=ValueError('dummy'))
-    assert isinstance(state, Savable)
-
-
-def test_finished_savable():
-    state = Finished(result='done', successful=True)
-    assert isinstance(state, Savable)
-
-
-def test_killed_savable():
-    state = Killed(msg=MessageBuilder.kill('kill it'))
-    assert isinstance(state, Savable)
-
-
-def test_subclass_command_savable():
-    class DummyCmd(Command):
-        pass
-
-    assert isinstance(DummyCmd(), Savable)
-
-
-def test_create_save_load(proc: DummyProcess):
+def test_create_savable(proc: DummyProcess):
     state = Created(proc, run_fn=proc.run)
+    assert isinstance(state, Savable)
+
+
+def test_running_savable(proc: DummyProcess):
+    state = Running(proc, run_fn=proc.run)
+    assert isinstance(state, Savable)
+
     ctx = LoadSaveContext(process=proc)
     saved_state = state.save()
     loaded_state = load(saved_state=saved_state, load_context=ctx)
@@ -78,6 +49,77 @@ def test_create_save_load(proc: DummyProcess):
     assert saved_state == saved_state2
 
 
-def test_running_save_load(proc: StateMachine):
-    state = Running(proc, run_fn=lambda: None)
+def test_waiting_savable(proc: DummyProcess):
+    state = Waiting(proc, done_callback=proc.run)
     assert isinstance(state, Savable)
+
+    ctx = LoadSaveContext(process=proc)
+    saved_state = state.save()
+    loaded_state = load(saved_state=saved_state, load_context=ctx)
+    saved_state2 = loaded_state.save()
+
+    assert saved_state == saved_state2
+
+
+def test_excepted_savable():
+    state = Excepted(exception=ValueError('dummy'))
+    assert isinstance(state, Savable)
+
+    saved_state = state.save()
+    loaded_state = load(saved_state=saved_state)
+    saved_state2 = loaded_state.save()
+
+    assert saved_state == saved_state2
+
+
+def test_finished_savable():
+    state = Finished(result='done', successful=True)
+    assert isinstance(state, Savable)
+
+    saved_state = state.save()
+    loaded_state = load(saved_state=saved_state)
+    saved_state2 = loaded_state.save()
+
+    assert saved_state == saved_state2
+
+
+def test_killed_savable():
+    state = Killed(msg=MessageBuilder.kill('kill it'))
+    assert isinstance(state, Savable)
+
+    saved_state = state.save()
+    loaded_state = load(saved_state=saved_state)
+    saved_state2 = loaded_state.save()
+
+    assert saved_state == saved_state2
+
+
+class DummyCmd(Command):
+    pass
+
+
+def test_subclass_command_savable():
+    cmd = DummyCmd()
+    assert isinstance(cmd, Savable)
+
+    saved = cmd.save()
+    loaded = load(saved_state=saved)
+    saved2 = loaded.save()
+
+    assert saved == saved2
+
+
+# FIXME: using pickle loader this should be able to be solved
+@pytest.mark.xfail(reason='the default loader can only load obj from python path')
+def test_subclass_command_savable_xfail():
+    class DummyCmdXfail(Command):
+        pass
+
+    cmd = DummyCmdXfail()
+    assert isinstance(cmd, Savable)
+
+    saved = cmd.save()
+    loaded = load(saved_state=saved)
+    saved2 = loaded.save()
+
+    assert saved == saved2
