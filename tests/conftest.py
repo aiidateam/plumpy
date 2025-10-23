0d72a394be4549df2d6465327fb6333a9756c0b4
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2025-10-23 04:20:01 +0100

    session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from plumpy.events import set_event_loop_policy, reset_event_loop_policy
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def custom_event_loop_policy():
     set_event_loop_policy()
     yield
